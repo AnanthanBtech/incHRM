1e1dad30e55d8146a39fd6535bc1066a56e7d8c7
Poongavanam <apoongavanam@ad.harman.com>
2019-08-21 12:58:39 +0530

    test

--- src/main/java/com/inc/user/helper/LoginHelper.java
+++ src/main/java/com/inc/user/helper/LoginHelper.java
@@ -33,11 +33,12 @@ public class LoginHelper {
 	 * @return
 	 */
 	public boolean isGreater(int num1, int num2){
-		
-		
+		testEnvironment(num1, num2);
+		// number of lines
 		System.out.println(num2);
 		System.out.println(num1+num2);
-		// number of lines
+		
+		System.out.println(num1+num2);
         return num1 > num2;
     }
 
